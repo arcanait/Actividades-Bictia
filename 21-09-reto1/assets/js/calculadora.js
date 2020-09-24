f86f1167354d5b34abb238139f8bdbcd54c5a2be
Daniel Mora <mora.felipe@insercor.com>
2020-09-23 21:54:40 -0500

    Se construye la calculadora con sus funcionalidade

---
+++ 21-09-reto1/assets/js/calculadora.js
@@ -0,0 +1,71 @@
+let teclas = ['%','CE','C','<-','1/x','x^2','raizx','/',7,8,9,'*',4,5,6,'-',1,2,3,'+','+/-',0,',','='];
+let numeros = [1,2,3,4,5,6,7,8,9,0]
+let teclado = document.getElementById('teclas');
+let primerArgumento = document.getElementById('espacio-primer-argumento');
+let dictado = [];
+for(let i = 0; i < teclas.length; i++){
+    if(typeof(teclas[i]) === 'number'){
+        let span = document.createElement('span');
+        span.setAttribute('id', `span${i}`);
+        span.setAttribute('class', `spanNumero`);
+        span.innerText = teclas[i];
+        teclado.appendChild(span);
+        span.addEventListener('click', () => {
+            dictado.push(teclas[i]);
+            primerArgumento.innerText = dictado.join().replace(/,/gi,'');
+        })
+    }else if(typeof(teclas[i]) === 'string'){
+        let span = document.createElement('span');
+        span.setAttribute('id', `span${i}`);
+        span.setAttribute('class', `span`);
+        span.innerText = teclas[i];
+        teclado.appendChild(span);
+        span.addEventListener('click', () => {
+            if(teclas[i] === '='){
+                let operacion = hacerOperacion(dictado);
+                primerArgumento.innerText = operacion
+            }else if(teclas[i] === 'C' || teclas[i] === 'CE'){
+                dictado = [];
+                primerArgumento.innerText = '';
+            }else if(teclas[i] === '<-'){
+                dictado.pop();
+                primerArgumento.innerText = dictado.join().replace(/,/gi,'');
+            }
+            else{
+                dictado.push(teclas[i]);
+                primerArgumento.innerText = dictado.join().replace(/,/gi,'');
+            }
+
+        })     
+    }
+}
+
+const hacerOperacion = (numeros) => {
+    let numeros1
+    let numeros2
+    let signo
+    numeros.forEach(numero => {
+        if(typeof(numero) !== 'number'){
+            signo = numero
+        }
+    })
+    numeros1 = numeros.join().replace(/,/gi, '').split(signo)[0]
+    numeros2 = numeros.join().replace(/,/gi, '').split(signo)[1]
+
+    switch(signo){
+        case '*':
+            return (typeof(numeros1 * numeros2) === 'number')? numeros1 * numeros2 : 'Math Error';
+        case '/':
+            return (typeof(numeros1 / numeros2) === 'number')? numeros1 / numeros2 : 'Math Error';
+        case '+':
+            return (typeof(numeros1 + numeros2) === 'number')? numeros1 + numeros2 : 'Math Error';
+        case '-':
+            return (typeof(numeros1 - numeros2) === 'number')? numeros1 - numeros2: 'Math Error';
+        case 'x^2':
+            return (typeof(Math.pow(numeros1, 2)) === 'number')? Math.pow(numeros1, 2) :  'Math Error';
+        case '1/x':
+            return (typeof(1 / numeros1) === 'number')? 1 / numeros1 : 'Math Error';
+        case 'raizx':
+            return (typeof(Math.sqrt(numeros1)) === 'number')? Math.sqrt(numeros1):'Math Error';
+    }
+}
